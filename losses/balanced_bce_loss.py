89afc2dec134188c65196944149833b0ac81010f
Rustam Khadipash <16683750+hadipash@users.noreply.github.com>
2023-02-01 15:23:12 +0800

    updated loss to run in the `graph` mode

--- losses/balanced_bce_loss.py
+++ losses/balanced_bce_loss.py
@@ -1,5 +1,5 @@
 import mindspore as ms
-from mindspore import nn, ops
+from mindspore import nn, ops, numpy as mnp
 
 
 class BalancedCrossEntropyLoss(nn.LossBase):
@@ -19,14 +19,24 @@ class BalancedCrossEntropyLoss(nn.LossBase):
         positive = gt * mask
         negative = (1 - gt) * mask
 
-        pos_count = positive.sum().astype(ms.int32)
-        neg_count = ops.minimum(negative.sum(), pos_count * self._negative_ratio + 1).astype(ms.int32)  # FIXME: + 1 when pos_count is 0
+        pos_count = positive.sum(axis=(1, 2), keepdims=True).astype(ms.int32)
+        neg_count = negative.sum(axis=(1, 2), keepdims=True).astype(ms.int32)
+
+        neg_count = ops.minimum(neg_count, pos_count * self._negative_ratio + 1).squeeze(axis=(1, 2))   # FIXME: + 1 when pos_count is 0
 
         loss = self._bce_loss(logits, gt, None)
 
         pos_loss = loss * positive
-        neg_loss = loss * negative
+        neg_loss = (loss * negative).view(loss.shape[0], -1)
 
-        neg_loss, _ = ops.top_k(neg_loss.view(-1), int(neg_count))
+        neg_vals, _ = ops.sort(neg_loss)
+        neg_index = ops.stack((mnp.arange(loss.shape[0]), neg_vals.shape[1] - neg_count), axis=1)
+        min_neg_score = ops.expand_dims(ops.gather_nd(neg_vals, neg_index), axis=1)
 
-        return (pos_loss.sum() + neg_loss.sum()) / (pos_count + neg_count + self._eps)
+        neg_loss_mask = (neg_loss >= min_neg_score).astype(ms.float32)  # filter values less than top k
+        neg_loss_mask = ops.stop_gradient(neg_loss_mask)
+
+        neg_loss = neg_loss_mask * neg_loss
+
+        return (pos_loss.sum() + neg_loss.sum()) / \
+               (pos_count.astype(ms.float32).sum() + neg_count.astype(ms.float32).sum() + self._eps)
