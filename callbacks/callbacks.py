1. switch the optimizer to SGD following the original approach 2. improve the callback to have more detailed outputs 3. fix image reading 4. fix incorrect metrics calculation during evaluation

callbacks/callbacks.py
@@ -1,16 +1,54 @@
+from pathlib import Path
+
+import numpy as np
+from tqdm import tqdm
+from mindspore import save_checkpoint
 from mindspore.train.callback import Callback
 
 
-class ValCallback(Callback):
-    def __init__(self, model, val_dataset):
+class EpochSummary(Callback):
+    def __init__(self, model, val_dataset, val_freq=5, folder='./ckpts'):
         self._model = model
         self._data = val_dataset
+        self._val_freq = val_freq
+        self._folder = Path(folder)
+        self._folder.mkdir(parents=True, exist_ok=True)
+        self._max_f = 0.
+        self._losses = []
+        # a placeholder for the train progress meter. The actual initialization is in `on_train_epoch_begin`
+        self._pbar = tqdm(disable=True)
+
+    def on_train_step_end(self, run_context):
+        self._losses.append(run_context.original_args().net_outputs.asnumpy())
+        self._pbar.update()
+        self._pbar.set_postfix_str(f'Loss: {self._losses[-1]:.4f}')
+
+    def on_train_epoch_begin(self, run_context):
+        self._pbar.close()
+        self._pbar = tqdm(total=run_context.original_args().batch_num, ncols=140)
+        self._pbar.set_description(f'Epoch {run_context.original_args().cur_epoch_num}')
+        self._losses = []
 
     def on_train_epoch_end(self, run_context):
         cb_params = run_context.original_args()
-        epoch_num = cb_params.cur_epoch_num
-        if epoch_num > 0 and epoch_num % 5 == 0:
-            metrics = self._model.eval(self._data)['Test']
-            print(f"Recall: {metrics['recall'].avg} "
-                  f"Precision: {metrics['precision'].avg} "
-                  f"Fmeasure: {metrics['fmeasure'].avg}")
+        epoch = cb_params.cur_epoch_num
+
+        loss = np.average(self._losses)
+        if epoch % self._val_freq == 0:
+            metrics = self._model.eval(self._data, dataset_sink_mode=False)['Eval']
+            log = f"Loss: {loss:.4f} | Recall: {metrics['recall'].avg:.4f} | " \
+                  f"Precision: {metrics['precision'].avg:.4f} | F-score: {metrics['fmeasure'].avg:.4f}"
+
+            if metrics['fmeasure'].avg > self._max_f:
+                self._max_f = metrics['fmeasure'].avg
+                file_name = str(self._folder / f"epoch{epoch}_l{loss:.4f}_r{metrics['recall'].avg:.4f}_"
+                                               f"p{metrics['precision'].avg:.4f}_f{metrics['fmeasure'].avg:.4f}.ckpt")
+                # Save the network model.
+                save_checkpoint(cb_params.train_network, ckpt_file_name=file_name, async_save=True)
+        else:
+            log = f"Loss: {loss:.4f}"
+
+        self._pbar.set_postfix_str(log)
+
+    def __exit__(self, *err):
+        self._pbar.close()

metrics/metric.py
@@ -185,7 +185,7 @@ class QuadMetric:
         }
 
 
-class MyMetric(nn.Metric):
+class DBNetMetric(nn.Metric):
     def __init__(self):
         super().__init__()
         self._post_process = SegDetectorRepresenter(box_thresh=0.6)
@@ -196,8 +196,8 @@ class MyMetric(nn.Metric):
         self._raw_metrics = []
 
     def update(self, *inputs):
-        preds, gt = inputs
-        gt = {'polys': gt[0].asnumpy(), 'ignore': gt[1].asnumpy()}
+        preds, polys, ignore = inputs
+        gt = {'polys': polys.asnumpy(), 'ignore': ignore.asnumpy()}
         boxes, scores = self._post_process(preds)
         self._raw_metrics.extend(self._metric.validate_measure(gt, (boxes, scores)))
 

models/model_utils.py
@@ -2,7 +2,7 @@ from mindspore import nn
 from mindspore.nn.learning_rate_schedule import LearningRateSchedule
 
 
-class CustomWithLossCell(nn.Cell):
+class DBNetWithLossCell(nn.Cell):
     def __init__(self, backbone, loss_fn):
         super().__init__()
         self._backbone = backbone
@@ -13,14 +13,15 @@ class CustomWithLossCell(nn.Cell):
         return self._loss_fn(output, labels)
 
 
-class CustomWithEvalCell(nn.Cell):
+class DBNetWithEvalCell(nn.Cell):
     def __init__(self, network):
         super().__init__()
         self._network = network
 
-    def construct(self, data, *labels):
+    def construct(self, data, label1, label2):
         output = self._network(data)
-        return output, labels
+        # TODO: perform boxes and scores extraction here?
+        return output, label1, label2
 
 
 class DynamicLR(LearningRateSchedule):